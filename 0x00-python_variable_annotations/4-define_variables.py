bdf197b36970a70eb9a8f85e91aebe8637a77516
lensonlovescode <lensonisinparis@gmail.com>
2025-05-14 11:17:42 +0300

    Annotated definitiond

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Contains annotated variable definitions with specific values
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "ALX"
