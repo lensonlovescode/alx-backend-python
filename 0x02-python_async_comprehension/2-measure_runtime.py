28047511b10b90ebd102431d02967662f90d45ff
lensonlovescode <lensonisinparis@gmail.com>
2025-05-16 13:48:14 +0300

    Always annotate

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Measures runtime for executing async_comprehension
     """
