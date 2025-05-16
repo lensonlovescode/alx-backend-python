e7c5a2d173d7f14a93fd2127ffe6f9fa6d467a35
lensonlovescode <lensonisinparis@gmail.com>
2025-05-16 10:48:16 +0300

    Always wait

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Imports wait_random from the previous python file and writes
+an async routine that spawns wait_random n times with the specified max_delay
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """
+    Spawns wait_random n times with a specific max_delay
+    """
+    listio = []
+    for i in range(n):
+        listio.append(await wait_random(max_delay))
+
+    return listio
